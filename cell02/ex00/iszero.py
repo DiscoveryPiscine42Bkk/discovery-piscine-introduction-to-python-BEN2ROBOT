2908975b856c6a6ef8319b296deeec6c2ad56c04
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-24 14:40:22 +0000

    iszero

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/env python
+enter_number = input("Enter a number: ").strip()
+if enter_number == "0":
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
