8a8ccab3d301510c29db9f97e281293f153a4e79
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-24 21:06:33 +0700

    Update whatsyourname.py

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,5 +1,5 @@
-print ("Hey, what's your first name?")
 first_name = input()
-print ("And your last name?")
+print ("Hey, what's your first name?" , first_name )
 last_name = input()
+print ("And your last name?" , last_name )
 print ("Well, pleased to meet you" , first_name + last_name)
