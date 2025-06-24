7ab7f0d87af3b9fe1c63a9b67aa634045dd4bd71
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-24 23:39:36 +0700

    Update isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/env python
+number = float(input("Enter a number: ").strip()) 
+if number > 0 :
+    print("This number is positive.")
+elif number < 0 :
+    print("This number is negative.")
+else : 
+    print ("This number is both positive and negative.")
