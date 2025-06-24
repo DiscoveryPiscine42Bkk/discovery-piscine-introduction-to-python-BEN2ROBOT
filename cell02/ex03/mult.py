586765a57d5154583ca33f7fc10359b3ded6d2b2
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-24 23:47:12 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env python
+number01 = float(input("Enter the first number : ").strip()) 
+number02 = float(input("Enter the second number : ").strip()) 
+result = number01 * number02 
+print (result)
+if result > 0 :
+    print("The result is positive.")
+elif result < 0 :
+    print("The result is negative.")
+else : 
+    print ("This result is  positive and negative.")
