78d9f97f2be6b850cd00cac8d5e3812d2c496e16
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-25 07:11:12 +0000

    multi

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+enter_number = int(input("Enter a number : ").strip())
+multiply_number = 0
+while multiply_number <= 9 :
+    result = enter_number * multiply_number
+    print(f"{multiply_number} x {enter_number} = {result}")
+    multiply_number += 1
 
