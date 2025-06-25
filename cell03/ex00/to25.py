dab8781e33770fcc480c68fa3004d79e98d985c3
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-25 05:57:34 +0000

    enter number

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+enter_number = int(input("Enter a number less than 25 : ").strip())
+if enter_number > 25:
+    print("Error")
+else:
+    while enter_number <= 25:
+        print(f"Inside the loop, my variable is {int(enter_number)}")
+        enter_number += 1
+
+
 
