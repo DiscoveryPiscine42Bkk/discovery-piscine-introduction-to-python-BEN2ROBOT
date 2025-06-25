c824fe306a3aa94a4916464f7ef70ee8ac457bd5
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-25 18:02:05 +0000

    play_with_arrays

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/env python3
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [x for x in original_array if x >= 5]
+add_new_array = [x + 2 for x in new_array]
+print (original_array)
+print (add_new_array)
