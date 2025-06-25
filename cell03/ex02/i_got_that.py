78acacf46d34fe1bec16f6a88e38773086eb7845
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-25 07:58:37 +0000

    i_got_that

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+enter = input("What you gotta say?: ")
+while enter:
+    if enter == "STOP":
+        break
+    else:
+        enter = input("I got that! Anything else: ")
 
