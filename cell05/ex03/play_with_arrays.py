c8e612eb9279a68d0f2ea2c165b75e7b882bb5b2
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-25 18:24:35 +0000

    sets

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/env python3
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [x for x in original_array if x >= 5]
+add_new_array = [x + 2 for x in new_array]
+no_duplicates = list(set(add_new_array))
+print (original_array)
+print (no_duplicates)
