39da4d583d86df7407fac54054543cd323eb4a2f
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-26 06:38:54 +0000

    parameter_matching

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,11 @@
-d
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) != 2 :
+    print ("none")
+matching = sys.argv[1]
+user_in = input("What was the parameter?")
+if user_in == matching :
+    print ("Good job!")
+else :
+    print ("Nope, sorry...")
+
