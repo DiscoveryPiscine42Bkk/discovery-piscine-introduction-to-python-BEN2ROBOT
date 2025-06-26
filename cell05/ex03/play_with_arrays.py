6660be75048aec7e51518c39e954c63a2de428ec
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-26 11:22:55 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -3,5 +3,5 @@ original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = [x for x in original_array if x >= 5]
 add_new_array = [x + 2 for x in new_array]
 no_duplicates = list(set(add_new_array))
-print (original_array)
-print (no_duplicates)
+print(original_array)
+print(f"{{{', '.join(str(x) for x in no_duplicates)}}}")
