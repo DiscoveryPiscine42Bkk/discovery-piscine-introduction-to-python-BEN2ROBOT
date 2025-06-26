5bd83abe26820c9a84a7da266048d7cac774ca77
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-26 02:17:46 +0000

    lower

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) > 1:
+    if sys.argv[1].islower() : 
+        print("none")
+    else : 
+        print(sys.argv[1].lower())
+else :
+    print("none")
