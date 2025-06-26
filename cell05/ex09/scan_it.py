0b888a3a24c9f039d8f6abfe5e1903e917820eba
BEN2ROBOT <119800497+BEN2ROBOT@users.noreply.github.com>
2025-06-26 06:10:10 +0000

    scan

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,12 @@
-
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword = sys.argv[1]
+    search_string = sys.argv[2]
+    count = search_string.count(keyword)
+    if count > 0:
+        print(count)
+    else:
+        print("none")
